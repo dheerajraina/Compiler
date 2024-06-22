0b3be9f498ff7d7485ffc8c44a8b055b9730f83e
Dheeraj Raina <dheerajraina55@gmail.com>
2024-06-22 16:36:46 +0530

    updated main file

--- src/main.c
+++ src/main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "lexer.h"
+#include "parser.h"
+#include "semantic.h"
+#include "codegen.h"
+#include "runner.h"
 
 char *readSourceFromFile(const char *filename)
 {
@@ -29,9 +34,76 @@ char *readSourceFromFile(const char *filename)
         return buffer;
 }
 
+const char *tokenTypeToString(TokenType type)
+{
+        switch (type)
+        {
+        case INT:
+                return "INT";
+        case IDENTIFIER:
+                return "IDENTIFIER";
+        case ASSIGN:
+                return "ASSIGN";
+        case COLON:
+                return "COLON";
+        case SEMICOLON:
+                return "SEMICOLON";
+        case PRINT:
+                return "PRINT";
+        case LPAREN:
+                return "LPAREN";
+        case RPAREN:
+                return "RPAREN";
+        case LITERAL_INT:
+                return "LITERAL_INT";
+        case END_OF_FILE:
+                return "END_OF_FILE";
+        default:
+                return "UNKNOWN_TYPE";
+        }
+}
+void printTokens(TokenList tokens)
+{
+        for (size_t i = 0; i < tokens.size; i++)
+        {
+                printf("Token Type: %s, Value: %s\n", tokenTypeToString(tokens.tokens[i].type), tokens.tokens[i].value);
+        }
+}
+
+void printAST(Node *node, int level)
+{
+        if (node == NULL)
+                return;
+
+        // Print current node
+        printf("%*sNode Type: %d, Value: %s\n", level * 2, "", node->type, node->value);
+
+        // Recursively print children
+        for (int i = 0; i < 2; i++)
+        {
+                if (node->children[i] != NULL)
+                {
+                        printAST(node->children[i], level + 1);
+                }
+        }
+}
+
 int main()
 {
         char *sourceCode = readSourceFromFile("test/test_source.txt");
+        TokenList tokens = tokenize(sourceCode);
+        printf("Tokens:\n");
+        printTokens(tokens);
+
+        Node *syntaxTree = parse(&tokens);
+        printf("\nAbstract Syntax Tree:\n");
+        printAST(syntaxTree, 0);
+
+        analyze(syntaxTree);
+        generate(syntaxTree);
+        printf("\n\n\n\n");
+        printf("Final Source Code Run\n");
+        run();
 
         return EXIT_SUCCESS;
 }
